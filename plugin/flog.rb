736beaee70903692be10f956ecc8f4d71ba97a06
Mark Lee <vim@lazymalevolence.com>
2015-02-15 22:34:39 -0800

    Style fixes

--- plugin/flog.rb
+++ plugin/flog.rb
@@ -17,9 +17,9 @@ class Flog
     end
 
     mass[file] = ast.mass
-    process ast
+    process(ast)
     return true
-  rescue RubyParser::SyntaxError, Racc::ParseError => e
+  rescue RubyParser::SyntaxError, Racc::ParseError
     return false
   end
 
@@ -44,11 +44,11 @@ def show_complexity(results = {})
   hide_medium  = VIM::evaluate('s:hide_medium')
   hide_low     = VIM::evaluate('s:hide_low')
 
-  VIM.command ":silent sign unplace * file=#{VIM::Buffer.current.name}"
-  VIM.command ":sign define FlogDummySign"
-  VIM.command ":sign place 9999 line=1 name=FlogDummySign file=#{VIM::Buffer.current.name}"
+  VIM.command(":silent sign unplace * file=#{VIM::Buffer.current.name}")
+  VIM.command(":sign define FlogDummySign")
+  VIM.command(":sign place 9999 line=1 name=FlogDummySign file=#{VIM::Buffer.current.name}")
 
-  results.each do |line_number, (score, method)|
+  results.each do |line_number, (score, _)|
     complexity = case score
       when 0..medium_limit          then "LowComplexity"
       when medium_limit..high_limit then "MediumComplexity"
@@ -57,8 +57,8 @@ def show_complexity(results = {})
     value = score >= 100 ? "9+" : score.to_i
     value = nil if (hide_low == 1 && value < medium_limit) || (hide_medium == 1 && value < high_limit)
     if value
-      VIM.command ":sign define #{value} text=#{value} texthl=Sign#{complexity}"
-      VIM.command ":sign place #{line_number} line=#{line_number} name=#{value} file=#{VIM::Buffer.current.name}"
+      VIM.command(":sign define #{value} text=#{value} texthl=Sign#{complexity}")
+      VIM.command(":sign place #{line_number} line=#{line_number} name=#{value} file=#{VIM::Buffer.current.name}")
     end
   end
 end
